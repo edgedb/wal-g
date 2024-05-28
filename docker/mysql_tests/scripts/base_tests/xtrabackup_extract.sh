2ff3fda94922653b3a7bf8454752ce794dc6cde7
Nikolay Antonov <ostinru@gmail.com>
2024-05-28 15:51:01 +0500

    fuck

--- docker/mysql_tests/scripts/base_tests/xtrabackup_extract.sh
+++ docker/mysql_tests/scripts/base_tests/xtrabackup_extract.sh
@@ -23,7 +23,7 @@ service mysql start
 mysql -e "CREATE TABLE sbtest.mytest (id int NOT NULL AUTO_INCREMENT, val varchar(80) DEFAULT NULL, PRIMARY KEY (id)) ENGINE=InnoDB COMPRESSION='zlib'"
 mysql -e "INSERT INTO sbtest.mytest(val) VALUES ('aaa')"
 mysql -e "INSERT INTO sbtest.mytest(val) VALUES ('bbb')"
-for i in $(seq 1 15); do
+for i in $(seq 1 4); do
   mysql -e "INSERT INTO sbtest.mytest(val) (SELECT concat(a.val, b.val) FROM sbtest.mytest as a cross join sbtest.mytest as b )"
 done
 sleep 1
